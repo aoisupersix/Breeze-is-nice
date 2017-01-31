0869ceab5c7fdf3bcbf37f6ca47fa5f9bab459ce
aoisupersix <aoi_supersix@yahoo.co.jp>
2017-01-31 20:09:10 +0900

    Initial Commit

---
+++ Breezeisnice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Breezeisnice
+//
+//  Created by 田中葵 on 2017/01/31.
+//  Copyright © 2017年 田中葵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
